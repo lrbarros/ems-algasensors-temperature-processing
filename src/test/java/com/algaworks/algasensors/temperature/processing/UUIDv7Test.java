187a0142a23da4413e64f961fdce143e9bc11554
luizRicardoJava <luizricardo@exactus.com.br>
2026-05-02 19:30:42 -0300

    primeiro comite

---
+++ src/test/java/com/algaworks/algasensors/temperature/processing/UUIDv7Test.java
@@ -0,0 +1,12 @@
+package com.algaworks.algasensors.temperature.processing;
+
+import org.junit.jupiter.api.Test;
+
+
+public class UUIDv7Test {
+
+    @Test
+    void deveGerarUUIDv7(){
+        TimeBasedEpochRendonGenerator
+    }
+}
